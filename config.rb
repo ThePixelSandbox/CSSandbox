8db6003332d664d4baf7a76122fe4c69e3adddb9
Joachim Damsgaard <nrone.dk@gmail.com>
2013-04-25 22:26:45 +0200

    File structure

---
+++ config.rb
@@ -0,0 +1,15 @@
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "scss"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :nested
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
