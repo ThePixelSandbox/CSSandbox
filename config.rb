201dc2e604b751bff889233b9ef210d4b30cd789
Joachim Damsgaard <nrone.dk@gmail.com>
2013-04-25 22:41:29 +0200

    Update config.rb
    Typo

--- config.rb
+++ config.rb
@@ -1,15 +1,16 @@
-# Set this to the root of your project when deployed:
-http_path = "/"
+# You should be fine with these settings, 
+# feel welcomed to make your own structure if needed.
 css_dir = "stylesheets"
 sass_dir = "scss"
+http_path = "/"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
-# output_style = :expanded or :nested or :compact or :compressed
+# Output: (:expanded) - (:nested) - (:compact) or (:compressed)
 output_style = :nested
 
-# To enable relative paths to assets via compass helper functions. Uncomment:
+# Relative patch assets
 relative_assets = true
 
-# To disable debugging comments that display the original location of your selectors. Uncomment:
+# Show comments to localize scss in css
 line_comments = false
